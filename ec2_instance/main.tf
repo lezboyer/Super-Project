65fa38bddf52c0fbf9b8b8df450b71386e35261d
Mouhamadou Lamine BOYE <m.lamineboye@gmail.com>
2024-06-15 07:52:50 +0200

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "jenkins_ak.pem" 
+  key_name      = "jenkins_ak" 
   tags = {
     Name = var.instance_name
   }
